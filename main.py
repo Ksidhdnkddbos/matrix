e51cf0888ad9da098cf2f198274c9116b8fa9ae3
Ahmed programmer <119659334+qithoniq@users.noreply.github.com>
2023-10-22 15:01:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+from pytgcalls import idle
+from config import call_py
+from MusicMatrix.التشغيل import arq
+
+        
+async def main():
+    await call_py.start()
+    print(
+        """
+    🥢------------------🥢
+🥡   | تم تشغيل ميوزك ماتركس | 🥡
+    🥢------------------🥢
+"""
+    )
+    await idle()
+    await arq.close()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
