ab5427184baaed59d14425407a774bb9f2114bc9
Ahmed programmer <119659334+qithoniq@users.noreply.github.com>
2023-11-04 14:50:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import asyncio
 from pytgcalls import idle
 from config import call_py
-from MusicMatrix.التشغيل import arq
-
-        
+from MusicTelethon.التشغيل import arq
 async def main():
     await call_py.start()
-    print(
-        """
-    🥢------------------🥢
-🥡 | تم تشغيل ميوزك ماتركس | 🥡
-    🥢------------------🥢
-"""
-    )
+    print("""    ------------------
+   | ميوزك ماتركس العربي شتغل ! |
+    ------------------"""    )
     await idle()
     await arq.close()
-
-
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
