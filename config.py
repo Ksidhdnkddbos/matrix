b4c40bfac973af32999b1705487567fab8eca5c1
Ahmed programmer <119659334+qithoniq@users.noreply.github.com>
2023-11-04 14:47:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,16 @@
 import os
-
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
-
+# للنشر المحلي
 if os.path.exists(".env"):
     load_dotenv(".env")
-
+# الفارات
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
-OWNER_NAME = os.getenv("OWNER_NAME")
-CHANNEL = os.getenv("CHANNEL")
-PHOTO_CH = os.getenv("PHOTO_CH")
-MATRIXTM = os.getenv("MATRIXTM", "")
+MATRIXTM = os.getenv("MATRIXTM", "$")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
-
-
-contact_filter = filters.create(
-    lambda _, __, message: (message.from_user and message.from_user.is_contact)
-    or message.outgoing
-)
-
+contact_filter = filters.create(    lambda _, __, message: (message.from_user and message.from_user.is_contact)    or message.outgoing)
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicMatrix"))
 call_py = PyTgCalls(bot)
